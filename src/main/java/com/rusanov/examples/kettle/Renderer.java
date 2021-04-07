b5cbf86422bf76d27171d43e4ca74765fb16a989
Nik <Nik@DESKTOP-8SURP8U>
2021-04-07 23:50:28 +0300

    teapot x translate 40

--- src/main/java/com/rusanov/examples/kettle/Renderer.java
+++ src/main/java/com/rusanov/examples/kettle/Renderer.java
@@ -11,23 +11,7 @@ final private GLU glu = new GLU();
     @Override
     public void init(GLAutoDrawable drawable) {
 
-        GL2 gl = drawable.getGL().getGL2();
 
-
-        gl.glClearColor(0f, 0f, 0f, 0f);
-        gl.glClearDepth(1f);
-        gl.glDepthFunc(GL2.GL_LESS);
-        gl.glEnable(GL2.GL_DEPTH_TEST);
-        gl.glShadeModel(GL2.GL_LUMINANCE32F);
-        gl.glMatrixMode(GL2. GL_PROJECTION );
-        gl.glLoadIdentity();
-       // camera perspective
-        glu.gluPerspective(-200f, 2f, 3f, 100f);
-        gl.glMatrixMode(GL2.GL_MODELVIEW);
-        gl.glEnable(GL2.GL_LIGHTING);
-        gl.glEnable(GL2.GL_LIGHT0);
-//        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE, new float[]{1f, 1f, 1f, 1f}, 0);
-//        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, new float[]{0f, 0f, 1f, 0f}, 0);
 
 
     }
@@ -43,19 +27,18 @@ final private GLU glu = new GLU();
         gl.glMatrixMode(GL2.GL_FOG_MODE);
         gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
-        gl.glTranslatef(0, 0, 10);
         GLUT glut = new GLUT();
         GLUT glut2 = new GLUT();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
         gl.glPushMatrix();
-        gl.glTranslatef(40f,0f,-5f);
-        glut.glutSolidTeapot(2);
+        gl.glTranslatef(40f,0f,0f);
+        glut.glutSolidTeapot(4);
         gl.glPopMatrix();
         gl.glPushMatrix();
 
         gl.glTranslatef(0f, 0f, -5f);
-        glut2.glutWireSphere(2,100,50);
+        glut2.glutWireSphere(3,100,50);
 
         //glut2.glutSolidTetrahedron();
 
@@ -64,7 +47,25 @@ final private GLU glu = new GLU();
 
     @Override
     public void reshape(GLAutoDrawable drawable, int x, int y, int weight, int height) {
+        GL2 gl = drawable.getGL().getGL2();
 
+
+        gl.glClearColor(0f, 0f, 0f, 0f);
+        gl.glClearDepth(1f);
+        gl.glDepthFunc(GL2.GL_LESS);
+        gl.glEnable(GL2.GL_DEPTH_TEST);
+        gl.glShadeModel(GL2.GL_LUMINANCE32F);
+        gl.glMatrixMode(GL2. GL_PROJECTION );
+        gl.glLoadIdentity();
+        // camera perspective
+        glu.gluPerspective(45f, 1.5f, 0.1f, 100f);
+        //gl.glFrustumf(0,0,100,100,2,100);
+        glu.gluLookAt(-50,-50,-30,50,50,50,100,100, 100);
+        gl.glMatrixMode(GL2.GL_MODELVIEW);
+        gl.glEnable(GL2.GL_LIGHTING);
+        gl.glEnable(GL2.GL_LIGHT0);
+//        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_DIFFUSE, new float[]{1f, 1f, 1f, 1f}, 0);
+//        gl.glLightfv(GL2.GL_LIGHT0, GL2.GL_POSITION, new float[]{0f, 0f, 1f, 0f}, 0);
     }
 
     public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
